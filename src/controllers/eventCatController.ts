3066ac5eb96d7b8ec9c70d3917c88b548cade64a
vinaarchi <vinaa.as4@gmail.com>
2024-12-18 11:22:08 +0700

    revamp eventCatController

--- src/controllers/eventCatController.ts
+++ src/controllers/eventCatController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "../config/prisma";
 
 export class EventCategoryController {
   //create a new event category
