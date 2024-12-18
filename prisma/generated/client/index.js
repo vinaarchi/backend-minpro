92a44d31525d7461969c45da79efe42f722eeb8e
vinaarchi <vinaa.as4@gmail.com>
2024-12-18 10:39:58 +0700

    revamp prisma

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -259,7 +259,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
