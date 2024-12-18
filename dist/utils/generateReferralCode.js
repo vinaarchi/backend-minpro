938889a5fc74184ae2eb9732a9193cf8ecde170a
Avina Anggraeni <vinaa.as4@gmail.com>
2024-12-18 10:36:47 +0700

    Delete dist directory

--- dist/utils/generateReferralCode.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateReferralCode = void 0;
-const generateReferralCode = () => {
-    return Math.random().toString(36).substring(2, 12).toUpperCase();
-};
-exports.generateReferralCode = generateReferralCode;
