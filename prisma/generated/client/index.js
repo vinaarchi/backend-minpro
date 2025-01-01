7f6bec78e3f28e66c310856d1e47593f2ca9366d
vinaarchi <vinaa.as4@gmail.com>
2025-01-01 23:04:45 +0700

    first clone vina

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -246,7 +246,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\Asus\\Documents\\Modul03\\git-minpro\\backend-minpro\\prisma\\generated\\client",
+      "value": "D:\\Full Stack Web Development Purwadhika\\JCWD-0412\\03_Backend\\mini-pro\\backend-minpro\\prisma\\generated\\client",
       "fromEnvVar": null
     },
     "config": {
@@ -260,12 +260,11 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\Asus\\Documents\\Modul03\\git-minpro\\backend-minpro\\prisma\\schema.prisma",
+    "sourceFilePath": "D:\\Full Stack Web Development Purwadhika\\JCWD-0412\\03_Backend\\mini-pro\\backend-minpro\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../..",
   "clientVersion": "6.1.0",
